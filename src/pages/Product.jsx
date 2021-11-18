5570740a03fb6ba625bccc2ee421da8a6bd165b2
Ciprian BRAT <ciprian.brat@indsoft.ro>
2021-11-18 12:12:14 +0200

    Delete link from router

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Layout from './../layout/Layout';
 import productsJSON from '../utils/products.json'
 import { connect } from 'react-redux';
